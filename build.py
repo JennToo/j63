11ccc62b9d0281ce2b33c7c89026040f87a29a99
Jennifer Wilcox <jennifer@nitori.org>
2025-03-01 16:17:15 -0600

    Add Quartus project building

--- build.py
+++ build.py
@@ -4,10 +4,12 @@ import concurrent.futures
 import dataclasses
 import graphlib
 import logging
+import multiprocessing
 import os
+import pathlib
+import shutil
 import subprocess
 import sys
-import multiprocessing
 
 
 @dataclasses.dataclass
@@ -18,6 +20,12 @@ class Tasks:
 
 PYTHON_SOURCES = [
     "build.py",
+]
+QUARTUS_PROJECT_FILES = [
+    "hw/quartus/j63.qpf",
+    "hw/quartus/j63.qsf",
+    "hw/quartus/j63.sdc",
+    "hw/quartus/j63_toplevel.vhd",
 ]
 
 
@@ -40,9 +48,15 @@ def main():
 
         def run_task(task):
             if task not in tasks.builders:
-                logging.error(f"No rule to make {task}")
-                sys.exit(1)
-            tasks.builders[task](tasks.dependencies[task])
+                fatal(f"No rule to make {task}")
+            dependencies = tasks.dependencies[task]
+            if all(os.path.exists(x) for x in [task] + dependencies):
+                task_stat = os.stat(task)
+                dep_stats = [os.stat(x) for x in dependencies]
+                if all(task_stat.st_mtime >= dep.st_mtime for dep in dep_stats):
+                    logging.info(f"{task} up to date")
+                    return task
+            tasks.builders[task](task=task, dependencies=dependencies)
             return task
 
         task_iter.prepare()
@@ -65,16 +79,37 @@ def build_task_graph():
         builders[name] = builder
         dependencies[name] = deps
 
-    rule("all", nop, ["black-check", "ruff-check"])
+    rule("all", nop, ["black-check", "ruff-check", "build/j63_quartus/meta-built"])
     rule("format", nop, ["black-fix", "ruff-fix"])
-    rule("black-check", lambda deps: run(["black", "--check"] + deps), PYTHON_SOURCES)
-    rule("ruff-check", lambda deps: run(["ruff", "check"] + deps), PYTHON_SOURCES)
-    rule("black-fix", lambda deps: run(["black"] + deps), PYTHON_SOURCES)
     rule(
-        "ruff-fix", lambda deps: run(["ruff", "check", "--fix"] + deps), PYTHON_SOURCES
+        "black-check",
+        lambda dependencies, **kwargs: run(["black", "--check"] + dependencies),
+        PYTHON_SOURCES,
     )
-    for python_source in PYTHON_SOURCES:
-        rule(python_source, file_exists, [])
+    rule(
+        "ruff-check",
+        lambda dependencies, **kwargs: run(["ruff", "check"] + dependencies),
+        PYTHON_SOURCES,
+    )
+    rule(
+        "black-fix",
+        lambda dependencies, **kwargs: run(["black"] + dependencies),
+        PYTHON_SOURCES,
+    )
+    rule(
+        "ruff-fix",
+        lambda dependencies, **kwargs: run(["ruff", "check", "--fix"] + dependencies),
+        PYTHON_SOURCES,
+    )
+    rule(
+        "build/j63_quartus/meta-built",
+        build_quartus_project,
+        QUARTUS_PROJECT_FILES + ["build/j63_quartus"],
+    )
+    rule("build/j63_quartus", mkdir, [])
+
+    for source in PYTHON_SOURCES + QUARTUS_PROJECT_FILES:
+        rule(source, file_exists, [])
 
     return Tasks(dependencies=dependencies, builders=builders)
 
@@ -87,8 +122,7 @@ def filter_tasks(tasks, requested):
         next_tasks_to_check = set()
         for task in tasks_to_check:
             if task not in tasks.dependencies:
-                logging.error(f"No rule to make {task}")
-                sys.exit(1)
+                fatal(f"No rule to make {task}")
             for dep in tasks.dependencies[task]:
                 next_tasks_to_check.add(dep)
         tasks_to_keep.update(tasks_to_check)
@@ -110,23 +144,102 @@ def parse_args():
     return parser.parse_args()
 
 
-def file_exists(paths):
-    for path in paths:
-        if not os.path.exists(path):
-            logging.error(f"No rule to make {path}")
-            sys.exit(1)
+def file_exists(dependencies, **kwargs):
+    for file_ in dependencies:
+        if not os.path.exists(file_):
+            fatal(f"No rule to make {file_}")
 
 
-def nop(paths):
+def mkdir(task, **kwargs):
+    os.makedirs(task, exist_ok=True)
+
+
+def nop(**kwargs):
     return
 
 
-def run(cmd):
+def build_quartus_project(task, dependencies, **kwargs):
+    build_dir = pathlib.Path(task).parent
+    qsf_file = None
+    qpf_file = None
+    src_files = []
+    for dep in dependencies:
+        if dep.endswith(".vhd") or dep.endswith(".sdc"):
+            src_files.append(pathlib.Path(dep))
+        elif dep.endswith(".qsf"):
+            qsf_file = pathlib.Path(dep)
+        elif dep.endswith(".qpf"):
+            qpf_file = pathlib.Path(dep)
+
+    qsf_contents = qsf_file.read_text()
+    for src_file in src_files:
+        if src_file.suffix == ".vhd":
+            type_ = "VHDL_FILE"
+        elif src_file.suffix == ".sdc":
+            type_ = "SDC_FILE"
+        else:
+            fatal(f"Unknown src file type {src_file}")
+        qsf_contents += f"set_global_assignment -name {type_} {src_file.absolute()}\n"
+
+    build_qsf_file = build_dir / qsf_file.name
+    build_qsf_file.write_text(qsf_contents)
+    shutil.copy(qpf_file, build_dir / qpf_file.name)
+
+    quartus = os.environ["QUARTUS_ROOTDIR"]
+    project = qsf_file.stem
+    run([f"{quartus}/quartus_sh", "--prepare", project], cwd=build_dir)
+    run(
+        [
+            f"{quartus}/quartus_map",
+            "--read_settings_files=on",
+            "--write_settings_files=off",
+            project,
+            "-c",
+            project,
+        ],
+        cwd=build_dir,
+    )
+    run(
+        [
+            f"{quartus}/quartus_fit",
+            "--read_settings_files=on",
+            "--write_settings_files=off",
+            project,
+            "-c",
+            project,
+        ],
+        cwd=build_dir,
+    )
+    run(
+        [
+            f"{quartus}/quartus_asm",
+            "--read_settings_files=on",
+            "--write_settings_files=off",
+            project,
+            "-c",
+            project,
+        ],
+        cwd=build_dir,
+    )
+    run([f"{quartus}/quartus_sta", project, "-c", project], cwd=build_dir)
+
+    touch(task)
+
+
+def touch(path):
+    pathlib.Path(path).write_text("")
+
+
+def run(cmd, cwd=None):
     logging.info(" ".join(cmd))
-    result = subprocess.run(cmd, check=False)
+    result = subprocess.run(cmd, check=False, cwd=cwd)
     if result.returncode != 0:
-        logging.error("Command failed: " + " ".join(cmd))
-        sys.exit(result.returncode)
+        fatal("Command failed: " + " ".join(cmd), result.returncode)
+
+
+def fatal(message, code=1):
+    logging.error(message)
+    sys.exit(code)
 
 
 if __name__ == "__main__":
